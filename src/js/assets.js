aa48a1fdd69bb970f162d2d7acb3861bf07be9d0
Jiseung Jung <roadrunner23@yonsei.ac.kr>
2021-10-28 21:41:28 +0900

    update image paths for gh-pages deploy

--- src/js/assets.js
+++ src/js/assets.js
@@ -1,21 +1,35 @@
 import * as PIXI from 'pixi.js'
+import fish from '../img/fish.png'
+import jellyfish from '../img/jellyfish.png'
+import turtle from '../img/turtle.png'
+import sea from '../img/sea.png'
+import bigCloud1 from '../img/big-cloud1.png'
+import bigCloud2 from '../img/big-cloud2.png'
+import bigCloud3 from '../img/big-cloud3.png'
+import smallCloud1 from '../img/small-cloud1.png'
+import smallCloud2 from '../img/small-cloud2.png'
+import smallCloud3 from '../img/small-cloud3.png'
+import smallCloud4 from '../img/small-cloud4.png'
+import smallCloud5 from '../img/small-cloud5.png'
+import boat from '../img/boat.png'
+import coin from '../img/coin.png'
 
 const loader = PIXI.Loader.shared
 
-loader.add('fish', 'img/fish.png')
-    .add('jellyfish', 'img/jellyfish.png')
-    .add('turtle', 'img/turtle.png')
-    .add('sea', 'img/sea.png')
-    .add('bigCloud1', 'img/big-cloud1.png')
-    .add('bigCloud2', 'img/big-cloud2.png')
-    .add('bigCloud3', 'img/big-cloud3.png')
-    .add('smallCloud1', 'img/small-cloud1.png')
-    .add('smallCloud2', 'img/small-cloud2.png')
-    .add('smallCloud3', 'img/small-cloud3.png')
-    .add('smallCloud4', 'img/small-cloud4.png')
-    .add('smallCloud5', 'img/small-cloud5.png')
-    .add('boat', 'img/boat.png')
-    .add('coin', 'img/coin.png')
+loader.add('fish', fish)
+    .add('jellyfish', jellyfish)
+    .add('turtle', turtle)
+    .add('sea', sea)
+    .add('bigCloud1', bigCloud1)
+    .add('bigCloud2', bigCloud2)
+    .add('bigCloud3', bigCloud3)
+    .add('smallCloud1', smallCloud1)
+    .add('smallCloud2', smallCloud2)
+    .add('smallCloud3', smallCloud3)
+    .add('smallCloud4', smallCloud4)
+    .add('smallCloud5', smallCloud5)
+    .add('boat', boat)
+    .add('coin', coin)
 
 loader.onProgress.add(handleProgress)
 
