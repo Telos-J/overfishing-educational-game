a924749167cdb5942a4db9e609b6a6a37faffe28
Jiseung Jung <roadrunner23@yonsei.ac.kr>
2021-10-28 21:48:29 +0900

    setup server

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "dist")));
+} else {
+    require("dotenv").config();
+}
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+    console.log(`Listening on http://localhost:${PORT}`)
+})
