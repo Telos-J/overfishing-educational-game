f0848e82200e0c4ff5c0b59d0abe9efbf51f74b1
Jiseung Jung <roadrunner23@yonsei.ac.kr>
2021-10-28 23:12:24 +0900

    fix server

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require('path')
 const app = express()
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "client/build")));
+    app.use(express.static(path.join(__dirname, "dist")));
 } else {
     require("dotenv").config();
 }
